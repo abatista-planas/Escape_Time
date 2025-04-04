6fcf77e990c31ebf7b872a95e61d36d5ee40ace8
abatista-planas <albgzz@mst.edu>
2025-04-04 00:25:36 -0500

    Refactoring code

---
+++ main.cpp
@@ -0,0 +1,92 @@
+#include <algorithm>
+#include <complex>
+#include <execution>
+#include <fstream>
+#include <iostream>
+
+constexpr double TH_NORM = 4.0;
+constexpr unsigned MAX_ITER = 1000;
+
+using namespace std;
+using cmplx = complex<double>;
+
+unsigned CalculateIterations(const cmplx z, const cmplx &c) {
+  unsigned iter = 0;
+
+  double x = z.real();
+  double y = z.imag();
+
+  for (;;) {
+    const double tmp1 = x;
+    const double tmp2 = y;
+    x *= x;
+    y *= y;
+    if (x + y >= TH_NORM || iter >= MAX_ITER) break;
+    x += -y + c.real();
+    y = 2 * tmp1 * tmp2 + c.imag();
+    ++iter;
+  }
+  return iter;
+}
+
+vector<unsigned> CreateFractalPal(const double cx, const double cy,
+                                  const unsigned size) {
+  // The domain consists of the region [-2,2] \times [-2,2]
+  const double dp = 4.0 / size;
+
+  vector<unsigned> data(size * size);
+  for_each(execution::par_unseq, data.begin(), data.end(),
+           [&](unsigned &pixel) {
+             const ptrdiff_t position = &pixel - &(data[0]);
+             const int col_index = position % size;
+             const int row_index = position / size;
+
+             double x = -2.0 + col_index * dp;
+             double y = -2.0 + row_index * dp;
+
+             pixel = CalculateIterations({x, y}, {cx, cy});
+           });
+  return data;
+}
+
+vector<unsigned> CreateFractalSeq(const double cx, const double cy,
+                                  const unsigned size) {
+  // The domain consists of the region [-2,2] \times [-2,2]
+  const double dp = 4.0 / size;
+
+  vector<unsigned> data(size * size);
+  for (auto &&pixel : data) {
+    const ptrdiff_t position = &pixel - &(data[0]);
+    const int col_index = position % size;
+    const int row_index = position / size;
+
+    double x = -2.0 + col_index * dp;
+    double y = -2.0 + row_index * dp;
+
+    pixel = CalculateIterations({x, y}, {cx, cy});
+  }
+  return data;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 6) {
+    cerr << "Wrong input parameters" << endl;
+    cerr << "Usage: ./AdrM <cx> <cy> <size> <output_file> <1-seq or 1-pal>"
+         << endl;
+    cerr << "   Ex: ./AdrM -0.74797 -0.072500001 1024 fractal.dat 1" << endl;
+    return -1;
+  }
+
+  const double cx = stod(argv[1]);
+  const double cy = stod(argv[2]);
+  const unsigned size = stoul(argv[3]);
+  const string output_file = argv[4];
+  const bool use_pal = stoi(argv[5]);
+  auto data =
+      use_pal ? CreateFractalPal(cx, cy, size) : CreateFractalSeq(cx, cy, size);
+
+  ofstream ofile(output_file, ios::binary);
+  ofile.write(reinterpret_cast<char *>(data.data()), data.size() * sizeof(unsigned));
+  ofile.close();
+  return 0;
+}
---
+++ plot.py
@@ -0,0 +1,63 @@
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+from math import sqrt
+from matplotlib.colors import LightSource, Normalize
+import matplotlib as mpl
+import struct
+from matplotlib import colors
+import math
+
+
+def show_data(ax, data, cmap, useLog: bool = False, save_img="", title=""):
+
+    if useLog:
+        data = np.log(data)
+
+    cmap = mpl.colormaps.get_cmap(cmap)
+    ls = LightSource(315, 45)
+    rgb = ls.shade(data, cmap)
+
+    im = ax.imshow(rgb, interpolation="bilinear")
+    if save_img != "":
+        plt.imsave(save_img, rgb)
+
+    # Use a proxy artist for the colorbar...
+    im = ax.imshow(data, cmap=cmap)
+    im.remove()
+
+    plt.colorbar(im, ax=ax)
+    ax.set_title(title, size="x-large")
+
+
+def load_data(fname):
+
+    with open(fname, "rb") as ifile:
+        data = np.frombuffer(buffer=ifile.read(), dtype=np.uint32)
+        S = int(sqrt(data.size))
+        return data.reshape(S, S)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fname", type=str, help="Input file")
+    parser.add_argument("--cmap", type=str, default="jet", help="Colormap")
+    parser.add_argument("--log", help="Use log scale", action="store_true")
+    parser.add_argument("--save_img", help="Save image", default="")
+    FLAGS, unparsed_args = parser.parse_known_args()
+    if len(unparsed_args):
+        print("Warning: unknow arguments {}".format(unparsed_args))
+
+    data = load_data(fname=FLAGS.fname)
+
+    fig = plt.figure()
+    show_data(
+        ax=plt.gca(),
+        data=data,
+        cmap=FLAGS.cmap,
+        title="Fractal",
+        useLog=FLAGS.log,
+        save_img=FLAGS.save_img,
+    )
+    fig.tight_layout()
+    plt.show()
